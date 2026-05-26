7cc2b666ec20f38bb62fd34f432ee495256a4872
anastarawneh <anastarawneh2001@icloud.com>
2026-05-27 02:51:21 +0300

    Bump version

--- PK_Script.lua
+++ PK_Script.lua
@@ -1,5 +1,5 @@
 -- DO NOT EDIT THE FIRST FIVE LINES, INCLUDING THE COMMENTS!
-Version = "1.5"
+Version = "1.5.1"
 Command = ""
 Args = {}
 -- YOU MAY EDIT ANYTHING ELSE :D
@@ -189,7 +189,7 @@ function export(sync)
         end
     end
     if sync then
-local dead = 0
+        local dead = 0
         paste = paste:sub(1, -3).."], ["
         for box = 15, 18 do
             for slot = 1, 30 do
@@ -198,12 +198,12 @@ local dead = 0
                 if data then
                     data = tostring(memory.readbyterange(pc_offset + (box_mon_size * (((box - 1) * 30) + slot - 1)), 0x88)):gsub("{", "["):gsub("}", "], ")
                     paste = paste..data
-dead = dead + 1
+                    dead = dead + 1
                 end
             end
         end
-if dead > 0 then
-        paste = paste:sub(1, -3)
+        if dead > 0 then
+            paste = paste:sub(1, -3)
         end
         paste = paste.."]]"
     end
